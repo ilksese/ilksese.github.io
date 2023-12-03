8600b3062ca593ab633e730be4deaf09e60fca82
ilksese <leeyrur@gmail.com>
2023-12-03 14:36:59 +0800

    样式调整

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,4 @@
-import React from 'react';
 import ReactDOM from 'react-dom/client';
-import 'antd/dist/antd.css';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
